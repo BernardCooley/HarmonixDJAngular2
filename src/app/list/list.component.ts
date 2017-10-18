5e71814d6a867e3308ff8acd7a8cc6f6438e21c9
BernardCooley <bernerdcooley@gmail.com>
2017-10-18 23:46:20 +0100

    current set list added to play set

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,33 +1,64 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Injectable, Component, OnInit, Input } from '@angular/core';
 import { ListService } from '../services/list/list.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
+  // inputs: ['list']
 })
+
+@Injectable()
 export class ListComponent implements OnInit {
 
-  private tracks: any;
+  // list: string;
+  @Input() listName: string;
+  private list: any;
   private showTitle: boolean;
+  _listService: ListService;
 
-  constructor(_listService: ListService) {
-
-    this.tracks = _listService.getTracks();
+  constructor(_listServiceIn: ListService) {
+    
+    // this.tracks = _listService.getTracks();
 
     this.showTitle = false;
 
   }
 
   ngOnInit() {
+
+    // console.log(this.list);
+
+    this.loadList(this.listName);
   }
 
-  addToLiveSet(event: Event): void {
+  loadList(listNameIn: string) {
+    if(listNameIn == 'currentSet') {
+      console.log('currentSet selected');
+      this.list = [
+        { 'id': 1, 'title': 'Acidic Sky', 'artist': 'Tape Twelve', 'key': '11B' },
+        { 'id': 2, 'title': 'Man with the red face', 'artist': 'Laurent Garnier', 'key': '05A' },
+        { 'id': 3, 'title': 'Brainstorm', 'artist': 'Carl Craig', 'key': '08B' }
+      ]
+    }
+    else if (listNameIn == 'allTracks') {
+      console.log('allTracks selected');
+      this.list = [
+        { 'id': 14, 'title': 'Man with the red face', 'artist': 'Laurent Garnier', 'key': '05A' },
+        { 'id': 15, 'title': 'Brainstorm', 'artist': 'Carl Craig', 'key': '08B' },
+        { 'id': 16, 'title': 'Acidic Sky', 'artist': 'Tape Twelve', 'key': '11B' },
+        { 'id': 17, 'title': 'Man with the red face', 'artist': 'Laurent Garnier', 'key': '05A' },
+        { 'id': 18, 'title': 'Brainstorm', 'artist': 'Carl Craig', 'key': '08B' }
+      ]
+    };
+  }
+
+  addToLiveSet(event: Event) {
     console.log('add button clicked');
     console.log(event);
   }
 
-  trackMenu(event: Event): void {
+  trackMenu(event: Event) {
     console.log('menu button clicked');
     console.log(event);
   }
